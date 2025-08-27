f8a3c6085732b133e5ddc2845c50e3c215301956
Sophie Rong <sophie.rong@gmail.com>
2025-08-26 17:05:49 -0700

    Set up website skeleton

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="h-screen">
+      <h1 className="text-5xl font-semibold text-zinc-800 dark:text-zinc-300">Sophie Rong</h1>
+      <h2 className="text-lg text-zinc-400">Creator, artist, developer.</h2>
+    </div>
+  )
+}
+
+export default Hero
