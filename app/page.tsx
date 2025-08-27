8ed6a457efee670e3055f758098be96cc49e1258
Sophie Rong <sophie.rong@gmail.com>
2025-08-26 19:14:58 -0700

    Tweak navbar UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Hero from "@/components/Hero";
-import { Navbar, NavBody, NavItems, MobileNav, MobileNavHeader, MobileNavMenu, MobileNavToggle, NavbarButton } from "@/components/ui/resizable-navbar";
 
 export default function Home() {
   return (
     <main className="relative justify-center items-center
       flex-col overflow-hidden mx-auto
-      px-10 md:px-30 lg:px-40"
+      px-10 md:px-20 lg:px-40"
     >
-      <div className="w-full">
+      <div className="w-full max-w-7xl">
         <Hero />
 
         <div className="mb-50">Hello</div>
--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -69,7 +69,7 @@ export const Navbar = ({ children, className }: NavbarProps) => {
     <motion.div
       ref={ref}
       // IMPORTANT: Change this to class of `fixed` if you want the navbar to be fixed
-      className={cn("sticky inset-x-0 top-10 z-40 w-full", className)}
+      className={cn("sticky inset-x-0 top-4 z-40 w-full", className)}
     >
       {React.Children.map(children, (child) =>
         React.isValidElement(child)
@@ -120,7 +120,7 @@ export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
     <motion.div
       onMouseLeave={() => setHovered(null)}
       className={cn(
-        "absolute inset-0 hidden flex-1 flex-row items-center justify-center space-x-2 text-sm font-medium text-zinc-600 transition duration-200 hover:text-zinc-800 lg:flex lg:space-x-2",
+        "absolute right-32 hidden flex-1 flex-row items-center justify-center space-x-2 text-sm font-medium text-zinc-600 transition duration-200 hover:text-zinc-800 lg:flex lg:space-x-2",
         className,
       )}
     >
@@ -224,9 +224,9 @@ export const MobileNavToggle = ({
   onClick: () => void;
 }) => {
   return isOpen ? (
-    <IconX className="text-black dark:text-white" onClick={onClick} />
+    <IconX className="text-black dark:text-white hover:cursor-pointer" onClick={onClick} />
   ) : (
-    <IconMenu2 className="text-black dark:text-white" onClick={onClick} />
+    <IconMenu2 className="text-black dark:text-white hover:cursor-pointer" onClick={onClick} />
   );
 };
 
@@ -237,7 +237,7 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <img
-        src="/vercel.svg"
+        src="/circle.svg"
         alt="logo"
         width={30}
         height={30}
